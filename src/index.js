ffa55469c1bcbc2b97fbbc7af0a64b4ed1d5d121
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-02-26 19:03:39 -0600

    starting exercise

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Step 1 Import the React and ReactDOM libraires 
+// Step 2 Get a reference to the div with ID root
+// Step 3 Tell React to take control of that element
+// Step 4 Create a component
+// Step 5 Show the component on the screen 
